4a2a78da4f8de62e575083469d274d278eb3d7dd
sayanok <rng.jhn.osca@gmail.com>
2023-06-18 19:09:57 +0900

    modify あとはロジックかくだけ

--- word-frequency-app/src/App.tsx
+++ word-frequency-app/src/App.tsx
@@ -1,26 +1,45 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { useState } from "react";
 
-function App() {
+const App: React.FC = () => {
+  const [inputText, setInputText] = useState("");
+  const [errorMessage, setErrorMessage] = useState("");
+
+  function onClickHandler() {
+    if (inputText) {
+      setErrorMessage("");
+      tallyWords();
+    } else {
+      setErrorMessage("英文を入力してね");
+    }
+  }
+
+  function tallyWords() {
+    const array = inputText.split(" ");
+    let count: Array<{ hoge: number }> = [];
+    const keys = Object.keys(count);
+
+    for (let n = 0; n < array.length; n++) {
+      let hoge = array[n];
+
+      if (!keys.includes(hoge)) {
+        count.push({ hoge: 2 });
+        console.log(true);
+      } else {
+        count.push({ hoge: 1 });
+        console.log(false);
+      }
+    }
+  }
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+      <p>英文を入力してください</p>
+      <textarea onChange={(e) => setInputText(e.target.value)}></textarea>
+      <button onClick={() => onClickHandler()}>Translate</button>
+      <p>結果</p>
+      {errorMessage}
+    </>
   );
-}
+};
 
 export default App;
